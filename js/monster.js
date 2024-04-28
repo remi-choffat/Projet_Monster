dfc3cc5fe41cda8fb708cbdae57644ee5f9c31c2
Rémi Choffat <remi.choffat@gmail.com>
2024-04-28 16:08:47 +0200

    Étape 3

--- js/monster.js
+++ js/monster.js
@@ -2,7 +2,9 @@
 let name;
 let life;
 let money;
-let awake = false;
+let awake = true;
+
+const sleepTime = 7000;
 
 
 // Fonction qui initialise le monstre
@@ -21,7 +23,7 @@ function showme() {
 
 // Variables globales et Handlers associés aux boutons
 document.getElementById("b1").onclick = function () {
-    init("Monstre", 100, 50);
+    go();
 };
 document.getElementById("b2").onclick = function () {
     run();
@@ -35,9 +37,6 @@ document.getElementById("b4").onclick = function () {
 document.getElementById("b5").onclick = function () {
     eat();
 }
-document.getElementById("b6").onclick = function () {
-    show();
-}
 document.getElementById("b7").onclick = function () {
     work();
 }
@@ -48,28 +47,81 @@ let actionbox = document.getElementById("actionbox");
 // Fonctions de déroulement de l'application (actions du monstre)
 
 function run() {
+    if (awake) {
+        if (life >= 1) {
+            life--;
+            log(name + " runs, and loses 1 life");
+            displayStatus(life, money, awake);
+        } else {
+            log(name + " doesn't have enough lives to run (1 needed)");
+        }
+    } else {
+        log(name + " is asleep");
+    }
 }
 
 function fight() {
+    if (awake) {
+        if (life >= 3) {
+            life -= 3;
+            log(name + " fights, and loses 3 lives");
+            displayStatus(life, money, awake);
+        } else {
+            log(name + " doesn't have enough lives to fight (3 needed)");
+        }
+    } else {
+        log(name + " is asleep");
+    }
 }
 
 function work() {
+    if (awake) {
+        if (life >= 1) {
+            life--;
+            money += 3;
+            log(name + " works, loses 3 lives, and wins 3 money unities");
+            displayStatus(life, money, awake);
+        } else {
+            log(name + " doesn't have enough lives to work (1 needed)");
+        }
+    } else {
+        log(name + " is asleep");
+    }
 }
 
 function sleep() {
+    awake = false;
+    log(name + " fell asleep (for " + (sleepTime / 1000) + " seconds)");
+    displayStatus(life, money, awake);
+    setTimeout(function () {
+        awake = true;
+        life++;
+        log(name + " woke up and won 1 life");
+        displayStatus(life, money, awake);
+    }, sleepTime);
 }
 
 function eat() {
-}
-
-function show() {
+    if (awake) {
+        if (money >= 3) {
+            money -= 3;
+            life += 2;
+            log(name + " eats, loses 3 money unities, and wins 2 lives");
+            displayStatus(life, money, awake);
+        } else {
+            log(name + " doesn't have enough money to eat (3 unities needed)");
+        }
+    } else {
+        log(name + " is asleep");
+    }
 }
 
 
 // Fonction de démarrage de l'application
 function go() {
-    init("Monstre", 100, 50);
+    init("Monstre", 10, 5);
     document.getElementById("b6").onclick = showme;
+    displayStatus(life, money, awake);
 }
 
 

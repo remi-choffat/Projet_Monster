f61931912a6caddf3a324cb0e92b4401f82c68cf
Rémi Choffat <remi.choffat@gmail.com>
2024-04-28 15:44:46 +0200

    Étape 2

--- js/monster.js
+++ js/monster.js
@@ -2,7 +2,7 @@
 let name;
 let life;
 let money;
-let awake = true;
+let awake = false;
 
 
 // Fonction qui initialise le monstre
@@ -15,11 +15,11 @@ function init(n, l, m) {
 
 // Fonction que affiche toutes les propriétés du monstre
 function showme() {
-    alert("Nom : " + name + "\nVie : " + life + "\nArgent : " + money + "\nÉtat : " + (awake ? "Réveillé" : "Endormi"));
+    log("Name : " + name + "  -  Life : " + life + "  -  Money : " + money + "  -  Status : " + (awake ? "Awake" : "Asleep"));
 }
 
 
-// Handlers associés aux boutons
+// Variables globales et Handlers associés aux boutons
 document.getElementById("b1").onclick = function () {
     init("Monstre", 100, 50);
 };
@@ -41,6 +41,8 @@ document.getElementById("b6").onclick = function () {
 document.getElementById("b7").onclick = function () {
     work();
 }
+let status = document.getElementById("status");
+let actionbox = document.getElementById("actionbox");
 
 
 // Fonctions de déroulement de l'application (actions du monstre)
@@ -64,10 +66,26 @@ function show() {
 }
 
 
+// Fonction de démarrage de l'application
 function go() {
     init("Monstre", 100, 50);
     document.getElementById("b6").onclick = showme;
 }
+
+
+// Fonction qui ajoute un message dans la console
+function log(message) {
+    let p = document.createElement("p");
+    p.innerHTML = message;
+    actionbox.insertBefore(p, actionbox.firstChild);
+}
+
+
+// Fonction qui affiche l'état du monstre
+function displayStatus(life, money, awake) {
+    status.innerHTML = "<li>Life : " + life + "</li><li>Money : " + money + "</li><li>" + (awake ? "Awake" : "Asleep") + "</li>";
+}
+
 
 // Programme principal : appel de la fonction go() au chargement de la page
 window.onload = go;
